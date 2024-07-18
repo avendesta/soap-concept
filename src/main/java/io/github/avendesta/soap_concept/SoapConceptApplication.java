4659b4b24fb1a1ffd038e6f17e29fc8e2d7f34c1
avendesta <53615807+avendesta@users.noreply.github.com>
2024-07-17 21:54:23 -0500

    from spring initalizer

---
+++ src/main/java/io/github/avendesta/soap_concept/SoapConceptApplication.java
@@ -0,0 +1,13 @@
+package io.github.avendesta.soap_concept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapConceptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/avendesta/soap_concept/SoapConceptApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.avendesta.soap_concept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapConceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
